95b724f007227de6399ccd5e7ccc6c69cf3410bb
Buldakov Mikhail <mishabul2003@gmail.com>
2021-06-24 11:21:27 +0300

    lesson 3

--- main.js
+++ main.js
@@ -26,11 +26,11 @@ const setting = {
 function getQuantityElementElements(height){
     return document.documentElement.clientHeight / height + 1;
 }
-console.log(getQuantityElementElements(200));
+
 
 function startGame(){
     start.classList.add('hide');
-
+    gameArea.innerHTML = '';
     for (let i = 0; i < getQuantityElementElements(100); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
@@ -46,8 +46,12 @@ function startGame(){
         enemy.style.top = enemy.y + 'px';
         gameArea.appendChild(enemy);
     }
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2 + 'px';
+    car.style.top = 'auto';
+    car.style.bottom = '15px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop
     requestAnimationFrame(playGame);
@@ -55,6 +59,8 @@ function startGame(){
 
 function playGame(){
     if (setting.start){
+        setting.score += setting.speed;
+        score.innerHTML ="SCORE: <br>" + setting.score;
         moveRoad();
         moveEnemy();
         if (keys.ArrowLeft && setting.x > 0){
@@ -69,6 +75,7 @@ function playGame(){
         if (keys.ArrowUp && setting.y > 15){
             setting.y-=setting.speed;
         }
+        console.log(keys);
         car.style.left = setting.x + 'px';
         car.style.top = setting.y + 'px';
         requestAnimationFrame(playGame);
@@ -77,13 +84,18 @@ function playGame(){
 
 function startRun(event){
     event.preventDefault();
-    keys[event.key] = true;
+    if (keys.hasOwnProperty(event.key)){
+        keys[event.key] = true;
+    }
 }
 
 function stopRun(event){
     event.preventDefault();
-    keys[event.key] = false;
+    if (keys.hasOwnProperty(event.key)){
+        keys[event.key] = false;
+    }
 }
+
 
 function moveRoad(){
     let lines = document.querySelectorAll('.line');
@@ -100,6 +112,16 @@ function moveRoad(){
 function moveEnemy(){
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function(enemy){
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = enemy.getBoundingClientRect();
+
+        if (carRect.top <= enemyRect.bottom &&
+        carRect.right >= enemyRect.left &&
+        carRect.left <= enemyRect.right &&
+        carRect.bottom >= enemyRect.top){
+            setting.start = false;
+            start.classList.remove('hide');
+        }
         enemy.y += setting.speed / 2;
         enemy.style.top = enemy.y + 'px'
 
